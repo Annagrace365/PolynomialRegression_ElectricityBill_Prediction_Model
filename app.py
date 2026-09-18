115eac451bd7e8b9d836522e46d2712665ccba00
Anna Grace Raju <148966297+Annagrace365@users.noreply.github.com>
2026-09-18 09:08:49 +0530

    Add electricity bill prediction app using Streamlit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import joblib
+from sklearn.preprocessing import PolynomialFeatures
+
+model=joblib.load("polynomial_regression_electricitybill_prediction_model.pkl")
+st.title("Electricity Bill Prediction Based On AC Units")
+ac_units=st.number_input("Enter AC Units : ", min_value=0.0, value=100.0)
+if st.button("Predict"):
+  poly=PolynomialFeatures()
+  ac_units_poly=poly.fit_transform([[ac_units]])
+  prediction=model.predict(ac_units_poly)
+  st.success(f"Predicted Electricity Bill : {prediction[0]:.2f}")
